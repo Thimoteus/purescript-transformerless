94659a6505230a9ae20c1c5e9b2ed6a7ccf51b8c
thimoteus <Thimoteus@users.noreply.github.com>
2018-09-26 11:14:19 -0700

    Add ParCont newtype

--- src/Control/Monad/Transformerless/Cont.purs
+++ src/Control/Monad/Transformerless/Cont.purs
@@ -9,7 +9,13 @@ module Control.Monad.Transformerless.Cont
 
 import Prelude
 
+import Control.Alt (class Alt)
+import Control.Alternative (class Alternative)
+import Control.Plus (class Plus)
+import Data.Maybe (Maybe(..))
 import Data.Newtype (class Newtype)
+import Effect (Effect)
+import Effect.Ref as Ref
 
 newtype Cont r a = Cont ((a -> r) -> r)
 
@@ -47,3 +53,58 @@ instance bindCont :: Bind (Cont r) where
   bind (Cont fa) k = Cont \ fb -> fa (\ a -> runCont (k a) fb)
 
 instance monadCont :: Monad (Cont r)
+
+-- | Parallel Cont
+
+newtype ParCont a = ParCont (Cont (Effect Unit) a)
+
+derive instance newtypeParCont :: Newtype (ParCont a) _
+
+derive instance functorParCont :: Functor ParCont
+
+instance applyParCont :: Apply ParCont where
+  apply (ParCont cab) (ParCont ca) = ParCont $ Cont \k -> do
+    rab <- Ref.new Nothing
+    ra <- Ref.new Nothing
+
+    runCont cab \ab -> do
+      ma <- Ref.read ra
+      case ma of
+        Just a -> k (ab a)
+        _ -> Ref.write (Just ab) rab
+    
+    runCont ca \a -> do
+      mab <- Ref.read rab
+      case mab of
+        Just ab -> k (ab a)
+        _ -> Ref.write (Just a) ra
+
+instance applicativeParCont :: Applicative ParCont where
+  pure = ParCont <<< pure
+
+instance altParCont :: Alt ParCont where
+  alt (ParCont ca) (ParCont cb) = ParCont $ Cont \k -> do
+    doneRef <- Ref.new false
+
+    runCont ca \a -> do
+      done <- Ref.read doneRef
+      unless done do
+        Ref.write true doneRef
+        k a
+    
+    runCont cb \b -> do
+      done <- Ref.read doneRef
+      unless done do
+        Ref.write true doneRef
+        k b
+
+instance plusParCont :: Plus ParCont where
+  empty = ParCont (Cont mempty)
+
+instance alternativeParCont :: Alternative ParCont
+
+sequential :: ParCont ~> Cont (Effect Unit)
+sequential (ParCont c) = c
+
+parallel :: Cont (Effect Unit) ~> ParCont
+parallel = ParCont
